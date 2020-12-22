8dca5f43d7e98ea7270f9c059950f7e463c339a8
Hayden Miedema <haydenmiedema@gmail.com>
2020-12-21 20:58:19 -0500

    initial commit

---
+++ Api/EpisodeFunctions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+using BlazorApp.Shared;
+
+namespace BlazorApp.Api
+{
+    public static class EpisodeFunctions
+    {
+        private static string GetEpisode(int temp)
+        {
+            return "";
+        }
+
+        [FunctionName("Episodes")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var json = System.IO.File.ReadAllText("data/workaholics.json");
+            var episodes = System.Text.Json.JsonSerializer.Deserialize<Episode[]>(json);
+            return new OkObjectResult(episodes);
+        }
+    }
+}
---
+++ Shared/Episode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class Episode
+    {
+        public int season { get; set; }
+        public int episode { get; set; }
+        public string title { get; set; }
+        public string directed_by { get; set; }
+        public string written_by { get; set; }
+        public string air_date { get; set; }
+    }
+}
