443c5bbdcbbc4d1d7a3ddbef94eb97bb43efc18a
Hayden Miedema <haydenmiedema@gmail.com>
2020-12-21 21:25:55 -0500

    change file path for data file

--- Api/EpisodeFunctions.cs
+++ Api/EpisodeFunctions.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Linq;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-
 using BlazorApp.Shared;
+using System.IO;
 
 namespace BlazorApp.Api
 {
@@ -20,9 +18,9 @@ namespace BlazorApp.Api
         [FunctionName("Episodes")]
         public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-            ILogger log)
+            ILogger log, ExecutionContext context)
         {
-            var json = System.IO.File.ReadAllText("data/workaholics.json");
+            var json = File.ReadAllText(Path.Combine(context.FunctionAppDirectory, "data/workaholics.json"));
             var episodes = System.Text.Json.JsonSerializer.Deserialize<Episode[]>(json);
             return new OkObjectResult(episodes);
         }
